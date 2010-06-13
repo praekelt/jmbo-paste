079b8c75a36c9c211722745c5496e574541cffd9
shaunsephton <shaunsephton@gmail.com>
2010-06-13 19:22:17 +0200

    refactor

--- panya_paste_templates/templates.py
+++ panya_paste_templates/templates.py
@@ -3,7 +3,3 @@ from paste.script.templates import Template, var
 class PanyaMinimalTemplate(Template):
     _template_dir = 'templates/panya_minimal'
     summary = 'Creates a buildout providing Django and the Panya base app only.'
-
-    vars = [
-        var('eggs', 'panya', default='panya'),
-    ]
