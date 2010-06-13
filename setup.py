4899a3fa290680909c94cdb98d640cb4e399ffdf
shaunsephton <shaunsephton@gmail.com>
2010-06-13 15:03:32 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panya-paste-templates',
+    version='0.0.1',
+    description='Python Paste templates creating Panya buildout environments.',
+    long_description = open('README.rst', 'r').read(),
+    author= open('AUTHORS', 'r').read(),
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/panya-paste-templates',
+    packages = find_packages(),
+    install_requires = [
+        'setuptools',
+        'PasteScript>=1.7',
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
