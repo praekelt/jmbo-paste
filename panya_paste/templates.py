8a369e153f1987a670b5431f8e060c8147577b4a
shaunsephton <shaunsephton@gmail.com>
2010-06-14 15:33:16 +0200

    deply template

--- panya_paste/templates.py
+++ panya_paste/templates.py
@@ -3,3 +3,7 @@ from paste.script.templates import Template, var
 class PanyaMinimalTemplate(Template):
     _template_dir = 'templates/panya_minimal'
     summary = 'Creates a buildout providing Django and the Panya base app only.'
+
+class PanyaDeployTemplate(Template):
+    _template_dir = 'templates/panya_deploy'
+    summary = 'Creates a buildout providing 3 Django instances (production, staging and qa), NGINX and FCGI control scripts, and a selection of Panya and Django apps.'
