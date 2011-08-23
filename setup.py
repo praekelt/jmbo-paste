ac5931b002d47eeeafed242f31e7f65bb01b00cd
Shaun Sephton <shaunsephton@gmail.com>
2011-08-23 11:26:59 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-paste',
-    version='0.1.4',
+    version='0.1.5',
     description='Python Paste templates creating Jmbo Buildout environments.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
